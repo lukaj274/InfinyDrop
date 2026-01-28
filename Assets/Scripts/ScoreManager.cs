aaa7c169639f8410488c5889279446923b81f5c3
Luka Jacenko <jluka8082@gmail.com>
2026-01-27 18:27:58 -0800

    made scoremultiplier editable from editor

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -5,12 +5,22 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour
 {
     public static int Score;
+    public static int ScoreMultiplier;
+    
     public TMP_Text scoreText;
+    public int scoreMultiplier;
+
+    private GameObject _player;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Score = 0;
+        ScoreMultiplier = scoreMultiplier;
+        _player = GameObject.Find("Player");
+        var controller = _player.GetComponent<PlayerController>();
+
+        controller.fallSpeed *= ScoreMultiplier;
     }
 
     // Update is called once per frame
